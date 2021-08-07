d88429331d4dceffeff805cbd7d531c460cb828f
haithai91 <78775708+haithai91@users.noreply.github.com>
2021-08-07 10:35:14 +0700

    #feature AWS CDK & LocalStack

---
+++ infra/bin/app.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import { ApplicationStack } from '../lib/ApplicationStack';
+
+const app = new cdk.App();
+new ApplicationStack(app, 'ApplicationStack');
