45ecf3df583d7c54c2ece61e8a8b717c4b26b962
haithai91 <78775708+haithai91@users.noreply.github.com>
2022-08-19 09:58:25 +0700

    fix review code

--- lib/s3/screenshot.ts
+++ lib/s3/screenshot.ts
@@ -1,8 +1,7 @@
 import * as s3 from '@aws-cdk/aws-s3';
-import { Bucket } from "@aws-cdk/aws-s3/lib/bucket";
 import { Stack } from '@aws-cdk/core';
 
-export const generateScreenShotBucket = (root: Stack): Bucket => {
+export const generateScreenShotBucket = (root: Stack): s3.Bucket => {
     return new s3.Bucket(root, 'screen-shot', {
         bucketName: 'screenshot',
         // https://docs.aws.amazon.com/AmazonS3/latest/userguide/serv-side-encryption.html
